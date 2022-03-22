fa41b32fec8df9c840109f3a9efd2696c5fa09dc
hieudx99 <hieudx1104@gmail.com>
2022-03-22 15:00:46 +0700

    update: config deploy tomcat

--- eParking/src/main/java/com/example/eparking/EParkingApplication.java
+++ eParking/src/main/java/com/example/eparking/EParkingApplication.java
@@ -2,9 +2,10 @@ package com.example.eparking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EParkingApplication {
+public class EParkingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EParkingApplication.class, args);
